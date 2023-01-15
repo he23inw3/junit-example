4c9d8ae76cde7fb98e14a425078465fdc99824f9
hiro72 <golden2finger@gmail.com>
2023-01-15 14:35:24 +0900

    detekt(MatchingDeclarationName)エラーを解消

--- src/main/kotlin/com/example/junitexample/JUnitExampleApplication.kt
+++ src/main/kotlin/com/example/junitexample/JUnitExampleApplication.kt
@@ -4,9 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class DockerTestApplication
+class JUnitExampleApplication
 
 @Suppress("SpreadOperator")
 fun main(args: Array<String>) {
-	runApplication<DockerTestApplication>(*args)
+	runApplication<JUnitExampleApplication>(*args)
 }
